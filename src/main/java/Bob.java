5fa298fe18b82f28fc9e100637c451edd8eb56d2
LZX-999 <157023573+LZX-999@users.noreply.github.com>
2025-08-20 13:19:53 +0800

    Bob.java: Level 0 renamed files, added greetings and exit

---
+++ src/main/java/Bob.java
@@ -0,0 +1,23 @@
+public class Bob {
+    public static void main(String[] args) {
+        String logo = " /$$$$$$$   /$$$$$$  /$$$$$$$ \n" + //
+                        "| $$__  $$ /$$__  $$| $$__  $$\n" + //
+                        "| $$  \\ $$| $$  \\ $$| $$  \\ $$\n" + //
+                        "| $$$$$$$ | $$  | $$| $$$$$$$ \n" + //
+                        "| $$__  $$| $$  | $$| $$__  $$\n" + //
+                        "| $$  \\ $$| $$  | $$| $$  \\ $$\n" + //
+                        "| $$$$$$$/|  $$$$$$/| $$$$$$$/\n" + //
+                        "|_______/  \\______/ |_______/ \n" + //
+                        "                              \n" + //
+                        "                              \n" + //
+                        "                              ";
+        System.out.println("Hello from\n" + logo);
+        String lines = "____________________________________________________________";
+        System.out.println(lines);
+        System.out.println("Hello! I'm Bob");
+        System.out.println("What can I do for you?");
+        System.out.println(lines);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(lines);
+    }
+}
