5ce9de8d0d8e9f8a0e53dde98e8826b8ef233a98
LZX-999 <157023573+LZX-999@users.noreply.github.com>
2025-08-20 13:31:21 +0800

    Bob.java: Level 1 added echoing and exit after receiving bye

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
         String logo = " /$$$$$$$   /$$$$$$  /$$$$$$$ \n" + //
@@ -17,7 +19,25 @@ public class Bob {
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
         System.out.println(lines);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(lines);
+        boolean run = true;
+        Scanner sc = new Scanner(System.in);
+        while (run) {
+            System.out.print("Enter text: ");
+            String input = sc.nextLine();
+            switch (input) {
+                case "bye":
+                    run = false;
+                    System.out.println(lines);
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println(lines);
+                    break;
+                default:
+                    System.out.println(lines);
+                    System.out.println(input);
+                    System.out.println(lines);
+            } 
+        }
+        sc.close();
+
     }
 }
